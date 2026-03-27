48c3526187932b1aaa2fe7a4f66395f4aa0fe9d1
diego04ba <diegodeb04@gmail.com>
2026-03-27 19:14:56 +0100

    modifiche file di aruco

--- ros2_ws/run.sh
+++ ros2_ws/run.sh
@@ -1,5 +1,6 @@
 xhost +
 docker run -it --rm --net host --ipc host --privileged \
+    --device=/dev/video0:/dev/video0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/root/.Xauthority \
     -e DISPLAY=$DISPLAY \
